72f9cd863772b11d26ee47d8d9feab463529f49d
FPStruck <109345588+FPStruck@users.noreply.github.com>
2022-10-18 17:29:23 +1100

    Main Commit

---
+++ A4 Resources 2022/BRLS/src/library/entities/PayFineTest.java
@@ -0,0 +1,69 @@
+package library.entities;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.text.ParseException;
+import java.util.Date;
+import library.payfine.PayFineControl;
+import library.payfine.PayFineUI;
+import library.returnItem.ReturnItemControl;
+import library.returnItem.ReturnItemUI;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class PayFineTest {
+	
+	Loan loan;
+	Patron patron;
+	Item item;
+	Library library;
+	Calendar calendar;
+	PayFineControl pfc;
+	PayFineUI pfui;
+	ReturnItemControl ric;
+	ReturnItemUI riui;
+	
+	@BeforeEach
+	void Setup() throws ParseException {
+		
+		library = Library.getInstance();
+		calendar = Calendar.getInstance();
+		patron = library.addPatron("T", "J", "EG", 1);
+		item = library.addItem("A", "T", "12", ItemType.BOOK);
+		ric = new ReturnItemControl();
+		pfc = new PayFineControl();
+		riui = new ReturnItemUI(ric);
+		pfui = new PayFineUI(pfc);
+		
+		
+		loan = library.issueLoan(item, patron);
+		calendar.incrementDate(4);
+		library.updateCurrentLoanStatus();
+		
+		riui.setReady();
+		ric.itemScanned(1);
+		ric.scanningCompleted();
+		riui.setInspecting();
+		riui.setCompleted();
+		
+		library.calculateOverDueFine(loan);
+		
+		//// Fine Check /////
+		library.dischargeLoan(loan, false);
+		
+		pfui.setReady();
+		pfui.setPaying();
+		pfc.cardSwiped(1);
+		pfc.payFine(loan.getFines());
+		pfui.setCompleted();
+	}
+	
+	@Test
+	void test() {	
+		assertEquals(patron.finesOwed(), loan.getFines());
+	}
+}
+
+
+
